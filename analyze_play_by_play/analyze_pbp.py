022ee9a0a6af8e7b76eb7eb41fcf09947e81b566
Ryan Davis <rd11490@gmail.com>
2019-05-15 18:00:35 -0400

    adding analyze pbp

--- analyze_play_by_play/analyze_pbp.py
+++ analyze_play_by_play/analyze_pbp.py
@@ -1,13 +1,19 @@
 import pandas as pd
+import os
 
 
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
-play_by_play = pd.read_csv('data/2017-18_pbp_fixed.csv')
 
+dirname = os.path.dirname(__file__)
+input_file = os.path.join(dirname, 'data/2017-18_pbp.csv')
+output_file = os.path.join(dirname, 'data/unique_pbp.csv')
+
+play_by_play = pd.read_csv(input_file)
 play_by_play_for_analysis = play_by_play[['EVENTMSGTYPE', 'EVENTMSGACTIONTYPE', 'HOMEDESCRIPTION', 'NEUTRALDESCRIPTION', 'VISITORDESCRIPTION']]
 play_by_play_for_analysis = play_by_play_for_analysis.fillna('')
+
 play_by_play_for_analysis['DESCRIPTION'] = play_by_play_for_analysis['HOMEDESCRIPTION'] + ' ' + play_by_play_for_analysis['NEUTRALDESCRIPTION'] + ' ' + play_by_play_for_analysis['VISITORDESCRIPTION']
 play_by_play_for_analysis = play_by_play_for_analysis[['EVENTMSGTYPE', 'EVENTMSGACTIONTYPE','DESCRIPTION']]
 
@@ -20,4 +26,4 @@ for event in unique_pbp['EVENTMSGTYPE'].unique():
     print(unique_pbp[unique_pbp['EVENTMSGTYPE'] == event])
 
 
-unique_pbp.to_csv('data/unique_pbp.csv', index=False)
+unique_pbp.to_csv(output_file, index=False)
