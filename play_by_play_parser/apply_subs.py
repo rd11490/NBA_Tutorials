97b461e37164612478be6b52894a6a9fb86331f2
Ryan Davis <rd11490@gmail.com>
2019-05-25 12:14:57 -0400

    working on parser

--- play_by_play_parser/apply_subs.py
+++ play_by_play_parser/apply_subs.py
@@ -3,13 +3,61 @@ import pandas as pd
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
-# Game Id
+# Constants
 game_id = '0041700404'
+event_type = 'EVENTMSGTYPE'
+event_subtype = 'EVENTMSGACTIONTYPE' 
+home_description = 'HOMEDESCRIPTION'
+neutral_description = 'NEUTRALDESCRIPTION'
+away_description = 'VISITORDESCRIPTION'
+period_column = 'PERIOD'
+game_clock = 'PCTIMESTRING'
+time_elapsed = 'TIME_ELAPSED'
+time_elapsed_period = 'TIME_ELAPSED_PERIOD'
 
-play_by_play = pd.read_csv(('{}_pbp.csv'.format(game_id)))
+import pandas as pd
+import os
 
 
-players_at_start_of_period = pd.read_csv(("{}_players_at_period.csv".format(game_id)))
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+
+dirname = os.path.dirname(__file__)
+input_play_by_play = os.path.join(dirname, './data/{}_pbp.csv'.format(game_id))
+input_players_on_court = os.path.join(dirname, './data/{}_players_at_period.csv'.format(game_id))
+
+
+play_by_play = pd.read_csv(input_play_by_play, index_col=False)
+
+def parse_time_elapsed(time_str, period):
+    max_minutes = 12 if period < 5 else 5
+    [minutes, sec] = time_str.split(':')
+    minutes = int(minutes)
+    sec = int(sec)
+    min_elapsed = max_minutes - minutes - 1
+    sec_elapsed = 60 - sec
+
+    return (min_elapsed * 60) + sec_elapsed
+
+
+def calculate_time_elapsed(row):
+    time_in_period = calculate_time_elapsed_period(row)
+    period = row[period_column]
+    if period > 4:
+        return (12 * 60 * 4) + ((period - 5) * 5 * 60) + time_in_period
+    else:
+        return ((period - 1) * 12 * 60) + time_in_period
+
+
+def calculate_time_elapsed_period(row):
+    return parse_time_elapsed(row[game_clock], row[period_column])
+
+play_by_play[time_elapsed] = play_by_play.apply(calculate_time_elapsed, axis=1)
+play_by_play[time_elapsed_period] = play_by_play.apply(calculate_time_elapsed_period, axis=1)
+
+
+players_at_start_of_period = pd.read_csv(input_players_on_court)
 
 def split_row(list_str):
     return [x.replace('[', '').replace(']', '').strip() for x in list_str.split(',')]
@@ -47,43 +95,43 @@ EVENTMSGTYPE Types:
 """
 
 def is_made_shot(row):
-    return row['EVENTMSGTYPE'] == 1
+    return row[event_type] == 1
 
 def is_missed_shot(row):
-    return row['EVENTMSGTYPE'] == 2
+    return row[event_type] == 2
 
 def is_free_throw(row):
-    return row['EVENTMSGTYPE'] == 3
+    return row[event_type] == 3
 
 def is_rebound(row):
-    return row['EVENTMSGTYPE'] == 4
+    return row[event_type] == 4
 
 def is_turnover(row):
-    return row['EVENTMSGTYPE'] == 5
+    return row[event_type] == 5
 
 def is_foul(row):
-    return row['EVENTMSGTYPE'] == 6
+    return row[event_type] == 6
 
 def is_violation(row):
-    return row['EVENTMSGTYPE'] == 7
+    return row[event_type] == 7
 
 def is_substitution(row):
-    return row['EVENTMSGTYPE'] == 8
+    return row[event_type] == 8
 
 def is_timeout(row):
-    return row['EVENTMSGTYPE'] == 9
+    return row[event_type] == 9
 
 def is_jump_ball(row):
-    return row['EVENTMSGTYPE'] == 10
+    return row[event_type] == 10
 
 def is_ejection(row):
-    return row['EVENTMSGTYPE'] == 11
+    return row[event_type] == 11
 
 def is_start_of_period(row):
-    return row['EVENTMSGTYPE'] == 12
+    return row[event_type] == 12
 
 def is_end_of_period(row):
-    return row['EVENTMSGTYPE'] == 13
+    return row[event_type] == 13
 
 
 ###########################
@@ -131,6 +179,7 @@ Offensive fouls: Offensive, Charge
 
 
 
+
 """
 eventActionType Types: Free Throws
 
@@ -146,19 +195,37 @@ Free Throw Types
 
 """
 def is_1_of_1(row):
-    return row['EVENTMSGACTIONTYPE'] == 13
+    return is_free_throw(row) and row[event_subtype] == 13
 
 def is_2_of_2(row):
-    return row['EVENTMSGACTIONTYPE'] == 13
+    return is_free_throw(row) and row[event_subtype] == 13
 
 def is_3_of_3(row):
-    return row['EVENTMSGACTIONTYPE'] == 13
+    return is_free_throw(row) and row[event_subtype] == 13
 
 def is_technical(row):
-    return row['EVENTMSGACTIONTYPE'] == 13
+    return is_free_throw(row) and row[event_subtype] == 13
 
 def is_last_free_throw(row):
-    is_free_throw(row) and (is_1_of_1(row) or is_2_of_2(row) or is_3_of_3(row))
+    return is_1_of_1(row) or is_2_of_2(row) or is_3_of_3(row)
+    
+def is_last_free_throw_made(row):
+    return is_last_free_throw(row) and 'MISS' not in row[home_description] and 'MISS' not in row[away_description]
+
+"""
+Is jump ball not at start of period
+"""
+
+def is_time_start_of_period(row):
+    if row[period_column] < 5:
+        return row[game_clock] == '12:00'
+    else:
+        return row[game_clock] == '5:00'
+
+
+def is_in_game_jump_ball(row):
+    return is_jump_ball(row) and not is_time_start_of_period(row)
+
 
 
 
@@ -185,24 +252,24 @@ What ends a possession?
 
 1. Made Shot (Need to account for And-1s)
 2. Defensive Rebound
-3. Turnover
+X 3. Turnover
 4. Last made free throw  (Ignore FT 1 of 1 on away from play fouls with no made shot)
 5. Jump Ball (ignore start of game, half, etc)
 """
 def isEndOfPossession(row):
     # Is turnover?
-    return is_turnover(row)
+    return is_turnover(row) or is_last_free_throw_made(row)
+
+
+print(play_by_play.head(10))
 
 
 
-
-
-
-possessions = []
-
-for row in play_by_play.iterrows():
-    row = row[1]
-    update_subs(row)
+# possessions = []
+#
+# for row in play_by_play.iterrows():
+#     row = row[1]
+#     update_subs(row)
 
 
 
--- play_by_play_parser/test.py
+++ play_by_play_parser/test.py
@@ -1,79 +1,27 @@
-class PlayByPlayMap:
-    def __init__(self):
-        self.data = {}
 
-    def add(self, event, action, input):
-        if not event in self.data.keys():
-            self.data[event] = {}
+def parse_time_elapsed(time_str, period):
+    max_minutes = 12 if period < 5 else 5
+    [minutes, sec] = time_str.split(':')
+    minutes = int(minutes)
+    sec = int(sec)
+    min_elapsed = max_minutes - minutes - 1
+    sec_elapsed = 60 - sec
 
-        if not action in self.data[event].keys():
-            self.data[event][action] = {}
+    out =  (min_elapsed * 60) + sec_elapsed
 
-        self.merge_map(self.data[event][action], input)
+    print('{} -> {}'.format(time_str, out))
 
-    def merge_map(self, old, new):
-        for k in new.keys():
-            if not k in old.keys():
-                old[k] = new[k]
-            else:
-                for n_k in new[k].keys():
-                    if not n_k  in old[k].keys():
-                        old[k][n_k] = new[k][n_k]
-                    else:
-                        old[k][n_k] = old[k][n_k] + new[k][n_k]
-
-    def make_sentance(self, event, action):
-        return ' '.join([self.take_highest(self.data[event][action][k]) for k in self.data[event][action].keys()])
-
-    def take_highest(self, position):
-        out = 'NONE'
-        cnt = -1
-        for k in position.keys():
-            if position[k] > cnt:
-                cnt = position[k]
-                out = k
-        return out
-
-def take_one_row(group):
-    return group.head(1)
-
-def parse_sentance(description):
-    if type(description) is str:
-        lower = description.lower()
-        subbed = re.sub(r'\([^)]*\)', '', lower)
-        arr = subbed.strip().split(' ')
-        str_map = {}
-        for ind, gram in enumerate(arr):
-            str_map[ind] = {gram: 1}
-        return str_map
-    return {}
-
-
-def print_play_type(pbp_map):
-    for event in sorted(pbp_map.data.keys()):
-        for action in sorted(pbp_map.data[event].keys()):
-            res = pbp_map.make_sentance(event, action)
-            print('Event: {}, Action: {}'.format(event, action))
-            print('Out: {}'.format(res))
+    return out
 
 
 
-play_by_play = pd.read_csv('data/2017-18_pbp_fixed.csv')
 
-play_by_play_for_analysis = play_by_play[['EVENTMSGTYPE', 'EVENTMSGACTIONTYPE', 'HOMEDESCRIPTION', 'VISITORDESCRIPTION']]
 
-pbp_map_home = PlayByPlayMap()
-pbp_map_away = PlayByPlayMap()
+parse_time_elapsed('12:00', 1)
+parse_time_elapsed('7:42', 1)
+parse_time_elapsed('0:00', 1)
+parse_time_elapsed('0:38', 1)
+parse_time_elapsed('0:00', 5)
+parse_time_elapsed('0:38', 5)
 
-def map_phrase(row):
-    pbp_map_home.add(row['EVENTMSGTYPE'], row['EVENTMSGACTIONTYPE'], parse_sentance(row['HOMEDESCRIPTION']))
-    pbp_map_away.add(row['EVENTMSGTYPE'], row['EVENTMSGACTIONTYPE'], parse_sentance(row['VISITORDESCRIPTION']))
-
-play_by_play_for_analysis.apply(map_phrase, axis=1)
-
-print("Home")
-print_play_type(pbp_map_home)
-
-print("Away")
-print_play_type(pbp_map_away)
 
